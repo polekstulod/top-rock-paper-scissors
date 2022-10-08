348a55b3dfba29fb36b90cfb78fc1b21568efc46
James Paul Tulod <paultulod@pm.me>
2022-10-08 19:23:11 +0800

    Add playRound function

--- app.js
+++ app.js
@@ -4,3 +4,27 @@ function getComputerChoice() {
 
   return choices[randomNumber]
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase()
+  computerSelection = computerSelection.toLowerCase()
+
+  if (playerSelection === computerSelection) return 'Tie Game'
+  else if (playerSelection === 'rock') {
+    if (computerSelection === 'paper') return 'You Lose, Paper beats Rock'
+    else if (computerSelection === 'scissors') return 'You Win, Rock beats Scissors'
+  } else if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') return 'You Win, Paper beats Rock'
+    else if (computerSelection === 'scissors') return 'You Lose, Scissors beats Paper'
+  } else if (playerSelection === 'scissors') {
+    if (computerSelection === 'paper') return 'You Win, Scissors beats Paper'
+    else if (computerSelection === 'rock') return 'You Lose, Rock beats Scissors'
+  } else {
+    return 'Invalid input'
+  }
+}
+
+const playerSelection = prompt()
+const computerSelection = getComputerChoice()
+
+console.log(playRound(playerSelection, computerSelection))
